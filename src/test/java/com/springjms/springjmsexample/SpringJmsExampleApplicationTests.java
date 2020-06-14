a3d3a3361ac30f23c9c2771bef13a70eea37a23a
Carl Saptarshi <carlsaptarshi@rentalcars.com>
2020-06-14 17:57:19 +0100

    first commit of example projecT

---
+++ src/test/java/com/springjms/springjmsexample/SpringJmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjms.springjmsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
